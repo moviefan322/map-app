9c88a0854f416836025ffddc355637c155f7073e
moviefan322 <philman202@yahoo.com>
2023-06-02 13:11:57 -0400

    uses axios to fetch coordinates

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,36 @@
+import axios from "axios";
+import "dotenv/config.js";
+
 const form = document.querySelector("form")!;
 const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY;
+
+type GoogleGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
 
 function searchAddressHandler(event: Event) {
   event.preventDefault();
   const enteredAddress = addressInput.value;
 
-  // send this to Google's API!
+  axios
+    .get<GoogleGeocodingResponse>(
+      `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+        enteredAddress
+      )}&key=${GOOGLE_API_KEY}`
+    )
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location!");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+    })
+    .catch((err) => {
+      alert(err.message);
+      console.log(err);
+    });
 }
 
-form.addEventListener("submit", (e) => {});
+form.addEventListener("submit", searchAddressHandler);
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,9 @@
-const path = require('path');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/app.ts',
+  mode: "development",
+  entry: "./src/app.ts",
   devServer: {
     static: [
       {
@@ -11,20 +12,27 @@ module.exports = {
     ],
   },
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/dist/'
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/dist/",
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-      }
-    ]
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+    ],
   },
+  plugins: [
+    // fix "process is not defined" error:
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+    }),
+  ],
   resolve: {
-    extensions: ['.ts', '.js']
-  }
+    extensions: [".ts", ".js"],
+    fallback: { os: false, crypto: false },
+  },
 };
